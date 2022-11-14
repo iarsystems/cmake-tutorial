c25d71fc10b0fc0e6532b224a8d2836057c37a64
Felipe Torrezan <felipe.torrezan@iar.com>
2022-11-14 11:10:35 +0100

    Added STM8 startup in module.asm

--- tests/module.asm
+++ tests/module.asm
@@ -2,11 +2,16 @@
   PUBLIC main
 
   PUBLIC __iar_program_start
+#if defined(__IASMSTM8__)
+  EXTERN CSTACK$$Limit
+  SECTION `.near_func.text`:CODE:NOROOT(0)
+#else
   EXTERN __iar_static_base$$GPREL
   SECTION CSTACK:DATA:NOROOT(4)
 
   SECTION `.cstartup`:CODE(2)
   CODE
+#endif
 __iar_program_start:
 main:
   NOP
