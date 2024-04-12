8bbfc286dd2e4a738a0662861b64be957f6b4538
Felipe Torrezan <54443595+felipe-iar@users.noreply.github.com>
2024-04-12 18:37:33 +0200

    Added TrustZone interactive example (#38)
    Added trustzone project

---
+++ examples/trustzone/secure/secure-hello.h
@@ -0,0 +1,35 @@
+/*
+  Copyright (c) 2018-2024, IAR Systems AB.
+
+  Header file for the `trustzone` example.
+    Contains definitions used in the following targets:
+    - `secure`
+    - `non-secure`
+*/
+
+#pragma once
+#pragma language = extended
+
+#if (__ARM_FEATURE_CMSE == 3U)
+#define CMSE_NS_CALL  __cmse_nonsecure_call
+#define CMSE_NS_ENTRY __cmse_nonsecure_entry
+#else
+#define CMSE_NS_CALL
+#define CMSE_NS_ENTRY
+#endif
+
+#define NON_SECURE_ENTRY_TABLE 0x00200000
+
+typedef CMSE_NS_CALL char * (*secure_goodbye_t)(void);
+
+typedef void (CMSE_NS_CALL *ns_func0)(void);
+
+typedef struct non_secure_init_t
+{
+  ns_func0 init;
+  void*    stack;
+  ns_func0 main;
+} non_secure_init_t;
+
+CMSE_NS_ENTRY void secure_hello(char * s);
+CMSE_NS_ENTRY void register_secure_goodbye(secure_goodbye_t goodbye);
