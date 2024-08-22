35908f2d56521e2381797bd2535dca9aa8170cb7
Kane Anderson <kanderson@q-pac.com>
2024-08-22 09:20:52 -0400

    feat: compile without src present in next architecture layer

---
+++ examples/libs-no-src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "my_crc.h"
+
+int main() {
+  do_it();
+  return 0;
+}
---
+++ examples/libs-no-src/toolchain-iar-9-arm.cmake
@@ -0,0 +1,8 @@
+set(ProgramFiles "$ENV{ProgramFiles}")
+set(IAR_TOOLCHAIN_ROOT "${ProgramFiles}/IAR Systems/Embedded Workbench 9.2")
+
+find_program(CMAKE_C_COMPILER iccarm HINTS "${IAR_TOOLCHAIN_ROOT}/arm/bin")
+find_program(CMAKE_CXX_COMPILER iccarm HINTS "${IAR_TOOLCHAIN_ROOT}/arm/bin")
+find_program(CMAKE_ASM_COMPILER iasmarm HINTS "${IAR_TOOLCHAIN_ROOT}/arm/bin")
+set(CSPY_CMD "${IAR_TOOLCHAIN_ROOT}/common/bin/CSpyBat.exe")
+
