6ca2f6203fffef28e8828b2ad4e20c6003f442b4
Ankit Khandeparkar <ankit.khandeparkar@iar.com>
2024-10-18 13:47:27 +0200

    working bxarm tutorial

--- tutorial/bxarm/bxarm.cmake
+++ tutorial/bxarm/bxarm.cmake
@@ -2,12 +2,13 @@
 
 # Set CMake for cross-compiling
 set(CMAKE_SYSTEM_NAME Generic)
+set(TOOLKIT_DIR /opt/iarsystems/bxarm)
 
-# Set CMake to use the IAR C/C++ Compiler from the IAR Embedded Workbench for Arm
+# Set CMake to use the IAR C/C++ Compiler from the IAR Build Tools for Arm
 # Update if using a different supported target or operating system
-set(CMAKE_ASM_COMPILER "C:/Program Files/IAR Systems/Embedded Workbench 9.3/arm/bin/iasmarm.exe")
-set(CMAKE_C_COMPILER   "C:/Program Files/IAR Systems/Embedded Workbench 9.3/arm/bin/iccarm.exe")
-set(CMAKE_CXX_COMPILER "C:/Program Files/IAR Systems/Embedded Workbench 9.3/arm/bin/iccarm.exe")
+set(CMAKE_ASM_COMPILER ${TOOLKIT_DIR}/arm/bin/iasmarm)
+set(CMAKE_C_COMPILER   ${TOOLKIT_DIR}/arm/bin/iccarm)
+set(CMAKE_CXX_COMPILER ${TOOLKIT_DIR}/arm/bin/iccarm)
 
 # Avoids running the linker during try_compile()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
@@ -16,11 +17,12 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 # Reasonably recent IAR products ships with ninja (https://ninja-build.org)
 # The CMake code block below tries to find it. If not found, 
 # manually install the desired build system in your operating system
-# Alternatively: set(CMAKE_MAKE_PROGRAM "C:/path/to/ninja.exe")
+# Alternatively: set(CMAKE_MAKE_PROGRAM "/usr/bin/ninja")
 if(CMAKE_GENERATOR MATCHES "^Ninja.*$")
   find_program(CMAKE_MAKE_PROGRAM
-    NAMES ninja.exe
+    NAMES ninja
     PATHS $ENV{PATH}
-          "C:/Program Files/IAR Systems/Embedded Workbench 9.3/common/bin"
+          /opt/iarsystems/bxarm/common/bin
     REQUIRED)
 endif()
+
