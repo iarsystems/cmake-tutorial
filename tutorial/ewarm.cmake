9e07cf5b3ae5695d9d8466dda6c6acae5afbf460
Felipe Torrezan <201077479+felipet-iar@users.noreply.github.com>
2025-04-24 11:06:16 +0200

    Update ewarm.cmake
    - Updated to EWARM 9.60.4

--- tutorial/ewarm.cmake
+++ tutorial/ewarm.cmake
@@ -4,10 +4,10 @@
 set(CMAKE_SYSTEM_NAME Generic)
 
 # Set CMake to use the IAR C/C++ Compiler from the IAR Embedded Workbench for Arm
-# Update if using a different supported target or operating system
-set(CMAKE_ASM_COMPILER "C:/Program Files/IAR Systems/Embedded Workbench 9.3/arm/bin/iasmarm.exe")
-set(CMAKE_C_COMPILER   "C:/Program Files/IAR Systems/Embedded Workbench 9.3/arm/bin/iccarm.exe")
-set(CMAKE_CXX_COMPILER "C:/Program Files/IAR Systems/Embedded Workbench 9.3/arm/bin/iccarm.exe")
+# Update the paths if using any different supported target/version
+set(CMAKE_ASM_COMPILER "C:/iar/ewarm-9.60.4/arm/bin/iasmarm.exe")
+set(CMAKE_C_COMPILER   "C:/iar/ewarm-9.60.4/arm/bin/iccarm.exe")
+set(CMAKE_CXX_COMPILER "C:/iar/ewarm-9.60.4/arm/bin/iccarm.exe")
 
 # Avoids running the linker during try_compile()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
@@ -21,6 +21,6 @@ if(CMAKE_GENERATOR MATCHES "^Ninja.*$")
   find_program(CMAKE_MAKE_PROGRAM
     NAMES ninja.exe
     PATHS $ENV{PATH}
-          "C:/Program Files/IAR Systems/Embedded Workbench 9.3/common/bin"
+          "C:/iar/ewarm-9.60.4/common/bin"
     REQUIRED)
 endif()
