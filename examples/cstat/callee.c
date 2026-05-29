d67f4a6519c59c3f22d0c0eefe521c3977747dd5
Felipe Torrezan <felipe.torrezan@qt.io>
2026-05-29 09:13:40 +0200

    Revert "Implement recursion limit in callee function" (#59)
    Revert "Implement recursion limit in callee function (#58)"
    
    This reverts commit 1933abb018c4a517858e2cfef6d58fa81408d642.

--- examples/cstat/callee.c
+++ examples/cstat/callee.c
@@ -3,13 +3,6 @@
 
 void callee(void)
 {
-  static unsigned int g_recursion_level = 0;
-  
-  ++g_recursion_level;
-
-  /* A limit to avoid stack overflow due to infinite recursion */ 
-  if (g_recursion_level < 10)
-    callee();
-  
-  while(1);
+  caller();
 }
+
