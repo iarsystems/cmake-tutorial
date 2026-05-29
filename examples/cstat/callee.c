1933abb018c4a517858e2cfef6d58fa81408d642
Felipe Torrezan <felipe.torrezan@qt.io>
2026-05-29 09:08:48 +0200

    Implement recursion limit in callee function (#58)
    Added recursion limit to prevent stack overflow.

--- examples/cstat/callee.c
+++ examples/cstat/callee.c
@@ -3,6 +3,13 @@
 
 void callee(void)
 {
-  caller();
-}
+  static unsigned int g_recursion_level = 0;
+  
+  ++g_recursion_level;
 
+  /* A limit to avoid stack overflow due to infinite recursion */ 
+  if (g_recursion_level < 10)
+    callee();
+  
+  while(1);
+}
