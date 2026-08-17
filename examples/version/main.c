1988805bda24d82eab36e2300164f93f449cfd73
Felipe Torrezan <felipe.torrezan@qt.io>
2026-08-17 15:18:52 +0200

    Updated examples/version

--- examples/version/main.c
+++ examples/version/main.c
@@ -2,6 +2,27 @@
 
 #include "version.h"
 
+#if !defined(App_VERSION_MAJOR)
+#define App_VERSION_MAJOR 0
+#warning "App_VERSION_MAJOR defined but empty. Fallback to 0."
+#endif
+
+#if !defined(App_VERSION_MINOR)
+#define App_VERSION_MINOR 0
+#warning "App_VERSION_MINOR defined but empty. Fallback to 0."
+#endif
+
+#if !defined(App_VERSION_PATCH)
+#define App_VERSION_PATCH 0
+#warning "App_VERSION_PATCH defined but empty. Fallback to 0."
+#endif
+
+#if !defined(App_VERSION)
+#define App_VERSION "0.0.0"
+#warning "App_VERSION_PATCH defined but empty. Fallback to 0.0.0."
+#endif
+
 void main() {
-  printf("%d.%d.%d\n", Project_VERSION_MAJOR, Project_VERSION_MINOR, Project_VERSION_PATCH);
+  printf("Integer version: %d.%d.%d\n", App_VERSION_MAJOR, App_VERSION_MINOR, App_VERSION_PATCH);
+  printf("String version: %s\n", App_VERSION);
 }
